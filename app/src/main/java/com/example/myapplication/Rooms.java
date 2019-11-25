b45a391098adc08872e3f9e980021786b9214b3b
Nikhil Baddam <nikhil@Nikhils-MBP.hsd1.mi.comcast.net>
2019-11-25 13:11:55 -0500

    started the constructor for the Rooms class - includes eid, room name

--- app/src/main/java/com/example/myapplication/Rooms.java
+++ app/src/main/java/com/example/myapplication/Rooms.java
@@ -3,13 +3,40 @@ package com.example.myapplication;
 import android.content.Context;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 import androidx.room.RoomDatabase;
 
+import org.json.JSONArray;
+
 @Entity(tableName = BookingDatabase.TABLE_NAME_BOOKING)
 public class Rooms implements Serializable{
+    private static final HashMap<String, String> eid2room = new HashMap<String, String>() {{
+        put("7909", "BLC 1030");
+        put("7910", "BLC 1031");
+        put("7911", "BLC 2003");
+        put("7912", "BLC 2005");
+        put("7913", "BLC 2006");
+        put("7914", "BLC 2007");
+        put("7915", "BLC 2010");
+        put("7916", "BLC 3010");
+        put("7917", "BLC 4031");
+        put("7918", "BLC 4043");
+        put("7919", "BLC 4045");
+        put("7920", "BLC 4047");
+        put("7921", "BLC 4049");
+        put("7922", "BLC 4051");
+        put("7923", "BLC 4053");
+        put("7924", "BLC 5010");
+    }};
+
+    public Rooms(int eid, JSONArray data) {
+        this.eid = eid;
+        this.name = eid2room.get(eid);
+    }
+
     @PrimaryKey(autoGenerate = true)
     public int eid;
 
